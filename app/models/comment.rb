c7cc47927e874e991d3f226eceab81dbfb9275eb
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-05-04 03:22:02 -0700

    Add latest 3 post methods, latest 5 comments and update counters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
   belongs_to :post
+
+  def update_comment_counter
+    post.update(comments_counter: post.comments.all.length)
+  end
+  
 end
