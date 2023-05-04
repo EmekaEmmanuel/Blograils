366c3edeac11f6612b7319ddc3410d7b698ae4d0
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-05-04 10:55:40 -0700

    Add unit rspec test and validsations to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
 
   after_save :update_post_counter
 
+  validates :title, presence: true
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   def latest_five_comments
     comments.order(created_at: :desc).limit(5)
   end
