438e9343ec344780b8853e49d56c677df4a3e435
chaseknis <chase6th@gmail.com>
2023-05-18 16:53:12 +0200

    Update routes.rb

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+    devise_for :users
+  
+    devise_scope :user do
+      root 'devise/sessions#new'
+    end
+  
+    resources :users, only: [:index, :show] do 
+      resources :posts, only: [:index, :show, :new, :create] do
+        resources :comments, only: [:new, :create] 
+        resources :likes, only: [:create]
+      end
+    end
+    
+  end
